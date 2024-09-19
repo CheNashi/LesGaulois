ea63e60bf6102c45983badf2fe8f8933b813963b
Ihsan <112517801+CheNashi@users.noreply.github.com>
2024-09-19 15:25:51 +0200

    Update scénario.java

--- src/histoire/scénario.java
+++ src/histoire/scénario.java
@@ -1,10 +1,14 @@
-package histoire;
-
-public class scénario {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+//package histoire;
+//
+//public class scenario {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		asterix.parler("Bonjour Ã  tous");
+//		minus.parler(" UN GAU... UN GAUGAU");
+//		while (minus.getForce()>1) {
+//			asterix.fraper(minus);
+//		}
+//	}
+//
+//}
