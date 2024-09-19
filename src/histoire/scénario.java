1d9b997adf394abe0de94e2c85caee073cbb30a8
Ikhsan Istabiev <stk4773a@ensfsi.onmicrosoft.com>
2024-09-19 15:10:53 +0200

    debut scenario

---
+++ src/histoire/scénario.java
@@ -0,0 +1,10 @@
+package histoire;
+
+public class scénario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/personnages/Romain.java
@@ -0,0 +1,44 @@
+package personnages;
+
+public class Romain {
+	private String nom;
+	private int force;
+
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+
+	private String prendreParole() {
+		return "Le romain" + nom + " : ";
+	}
+
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aie!");
+		} else {
+			parler("J'abandonne");
+		}
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Romain [nom=" + nom + ", force=" + force + "]";
+	}
+
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 10);
+		System.out.println(minus);
+	}
+}
