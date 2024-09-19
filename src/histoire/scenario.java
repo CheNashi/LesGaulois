08a11e6aa483154873f0957365f4ef7785b3f7f2
Ikhsan Istabiev <stk4773a@ensfsi.onmicrosoft.com>
2024-09-19 15:24:05 +0200

    debut scenario

--- src/histoire/scenario.java
+++ src/histoire/scenario.java
@@ -1,10 +1,14 @@
-package histoire;
-
-public class scenario {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+//package histoire;
+//
+//public class scenario {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		asterix.parler("Bonjour à tous");
+//		minus.parler(" UN GAU... UN GAUGAU");
+//		while (minus.getForce()>1) {
+//			asterix.fraper(minus);
+//		}
+//	}
+//
+//}
